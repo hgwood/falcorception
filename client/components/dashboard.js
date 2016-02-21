6df316c5416adb805b8a02294d65c3a0cf2b8b49
Hugo Wood <hx4.5@free.fr>
2016-02-21 23:07:05 +0100

    refactor: removed dashboard component in favor of css only

--- client/components/dashboard.js
+++
@@ -1,4 +0,0 @@
-angular.module("falcorception.dashboard", []).component("dashboard", {
-  template: '<div ng-transclude></div>',
-  transclude: true,
-})
--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ angular.module("falcorception", [
   "ngRoute",
   "falcorception.app",
   "falcorception.jumbotron",
-  "falcorception.dashboard",
   "falcorception.metric",
   "falcorception.apiList",
   "falcorception.api",
